60c67cdb87d9ffa9c2ba2ef1e774a70ba19e0587
Mehdi <mehdi.regragui@ummahcharity.org>
2021-11-11 19:10:07 +0100

    add icons navbar

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,9 +1,26 @@
 import "./topbar.scss";
+import PersonIcon from "@mui/icons-material/Person";
+import EmailIcon from "@mui/icons-material/Email";
 
 export default function Topbar() {
   return (
     <div className="topbar">
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            MR
+          </a>
+          <div className="itemContainer">
+            <PersonIcon className="icon" />
+            <span>06 01 01 01 01</span>
+          </div>
+          <div className="itemContainer">
+            <EmailIcon className="icon" />
+            <span>mehdi.regragui@live.fr</span>
+          </div>
+        </div>
+        <div className="right">right</div>
+      </div>
     </div>
   );
 }
