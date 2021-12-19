c2e87438bd554966d626b36e92a0fc54c664734c
root <root@DESKTOP-JP2BC3D.localdomain>
2021-12-19 19:16:20 +0100

    modify burger button and brand img

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -8,7 +8,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            {"<Mr>"}
+            <img src="/assets/img/mr_dev_logo.png" alt="logo" />
           </a>
           {/* <div className="itemContainer">
             <PersonIcon className="icon" />
