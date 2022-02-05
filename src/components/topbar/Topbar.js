b0d3ea6e103ddf68356e99ab3b21aa4fafcb297e
Mehdi-78 <regragui.mehdii@gmail.com>
2022-02-06 00:14:40 +0100

    change the previous  navbar for a navbar with a indicator slider effect

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -4,29 +4,10 @@ import EmailIcon from "@mui/icons-material/Email";
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className={"topbar " + (menuOpen && "active")}>
-      <div className="wrapper">
-        <div className="left">
-          <a href="#intro" className="logo">
-            <img src="/assets/img/mr_dev_logo.png" alt="logo" />
-          </a>
-          {/* <div className="itemContainer">
-            <PersonIcon className="icon" />
-            <span>06 01 01 01 01</span>
-          </div>
-          <div className="itemContainer">
-            <EmailIcon className="icon" />
-            <span>mehdi.regragui@live.fr</span>
-          </div> */}
-        </div>
-        <div className="right">
-          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-            <span className="line1"></span>
-            <span className="line2"></span>
-            <span className="line3"></span>
-          </div>
-        </div>
-      </div>
+    <div className="left">
+      <a href="#intro" className="logo">
+        <img src="/assets/img/mr_dev_logo.png" alt="logo" />
+      </a>
     </div>
   );
 }
