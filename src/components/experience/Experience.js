357ba4e28be7dedde2ebc531b23a25b3b1852a86
Mehdi-78 <regragui.mehdii@gmail.com>
2022-02-10 01:59:29 +0100

    add experience cards section

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,9 +1,23 @@
 import "./experience.scss";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { jobs } from "../../data/jobs";
+import { Modal, Button } from "react-bootstrap";
+import ScrollContainer from "react-indiana-drag-scroll";
+import SwipeIcon from "@mui/icons-material/Swipe";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Experience(props) {
   const ref = useRef();
   const rootMargin = "-100px";
+  const [activeModal, setActiveModal] = useState(null);
+  const [show, setShow] = useState(false);
+
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+
+  const handleClose = () => setActiveModal(null);
+  const handleShow = (e, key) => setActiveModal(key);
+
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
@@ -18,5 +32,53 @@ export default function Experience(props) {
     ref.current && observer.observe(ref.current);
   }, []);
 
-  return <div className="experience" id="experience" ref={ref}></div>;
+  return (
+    <div className="experience" id="experience" ref={ref}>
+      <ScrollContainer className="jobs scroll-container">
+        {jobs.map((data, key) => {
+          return (
+            <div className="job" key={key}>
+              <h3>{data.company}</h3>
+              <p className="title">{data.title}</p>
+              <p className="date">
+                {data.start_date} - {data.end_date}
+              </p>
+              <p className="location">{data.location}</p>
+              <p className="resume">{data.resume}</p>
+              <div className="btnContainer">
+                <Button
+                  id={key}
+                  className="detailsBtn"
+                  onClick={(e) => handleShow(e, key)}
+                >
+                  Détails
+                </Button>
+              </div>
+              <Modal id={key} show={activeModal === key} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>
+                    {data.company}{" "}
+                    <p className="date">
+                      {data.start_date} - {data.end_date}
+                    </p>
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <div dangerouslySetInnerHTML={{ __html: data.details }} />
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button className="closeBtn" onClick={handleClose}>
+                    Fermer
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </div>
+          );
+        })}
+      </ScrollContainer>
+      <div className="swipeDiv">
+        <SwipeIcon className="swipeIcon" />
+      </div>
+    </div>
+  );
 }
