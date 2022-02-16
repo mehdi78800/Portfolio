96cea8919dbd1a0443c3f8bc5f66796d203ee75b
Mehdi-78 <regragui.mehdii@gmail.com>
2022-02-16 21:40:23 +0100

    initialise emailjs

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,12 @@
 import "./contact.scss";
 import React, { useEffect, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { config } from "../../config/config.js";
 
 export default function Contact(props) {
   const ref = useRef();
+  const form = useRef();
+
   const rootMargin = "-100px";
   const { parentCallback } = props;
 
@@ -20,5 +24,37 @@ export default function Contact(props) {
     ref.current && observer.observe(ref.current);
   }, [parentCallback]);
 
-  return <div className="contact" id="contact" ref={ref}></div>;
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        config.serviceID,
+        config.templateID,
+        form.current,
+        config.userID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="contact" id="contact" ref={ref}>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
 }
