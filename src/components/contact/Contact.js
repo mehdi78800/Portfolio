242443f92092481e3f05e0808f5198d28fa2213e
Mehdi-78 <regragui.mehdii@gmail.com>
2022-02-17 01:57:21 +0100

    emailjs + contact links

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,6 +2,7 @@ import "./contact.scss";
 import React, { useEffect, useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { config } from "../../config/config.js";
+import { Form, Button } from "react-bootstrap";
 
 export default function Contact(props) {
   const ref = useRef();
@@ -46,15 +47,89 @@ export default function Contact(props) {
 
   return (
     <div className="contact" id="contact" ref={ref}>
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
+      <div className="container" id="form-container">
+        <div className="row">
+          <div className="col-10 col-xl-6 form-div">
+            <Form ref={form} onSubmit={sendEmail}>
+              <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>Nom</Form.Label>
+                <Form.Control type="text" name="user_name" placeholder="Nom" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Label>Adresse email</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="user_email"
+                  placeholder="email"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formMessage">
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  name="message"
+                  placeholder="Votre message"
+                />
+              </Form.Group>
+              <div className="btnContainer">
+                <Button className="detailsBtn" type="submit">
+                  Envoyer
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+      <div className="container" id="contact-container">
+        <div className="row">
+          <div className="col-10 col-xl-6 contact-div">
+            <div className="col-3">
+              <a
+                href="https://www.linkedin.com/in/mehdi-regragui"
+                target="_blank"
+              >
+                <img
+                  className="social-icon"
+                  src="/assets/img/contact_icons/linkedin.png"
+                  alt="linkedin"
+                />
+              </a>
+            </div>
+            <div className="col-3">
+              <a href="https://github.com/mehdi78800" target="_blank">
+                <img
+                  className="social-icon"
+                  src="/assets/img/contact_icons/github.png"
+                  alt="github"
+                />
+              </a>
+            </div>
+            <div className="col-3">
+              <a href="tel:0766714446" target="_blank">
+                <img
+                  className="social-icon"
+                  src="/assets/img/contact_icons/phone.png"
+                  alt="phone"
+                />
+              </a>
+            </div>
+            <div className="col-3">
+              <a
+                href="/assets/img/cv_regragui_mehdi_developpeur_web.pdf"
+                target="_blank"
+                download
+              >
+                <img
+                  className="social-icon"
+                  src="/assets/img/contact_icons/cv.png"
+                  alt="cv"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
